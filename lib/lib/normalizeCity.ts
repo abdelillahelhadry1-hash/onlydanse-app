4c106d55abc2ac6b8354556bb2c3488f5967168d
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-01-27 13:47:01 +0000

    Delete lib/lib/normalizeCity.ts

--- lib/lib/normalizeCity.ts
+++
@@ -1,8 +0,0 @@
-export function normalizeCity(input: string) {
-  return input
-    .trim()
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/\s+/g, " ");
-}
