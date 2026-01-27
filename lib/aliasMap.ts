55ed638a5dd196b24f5efd299c2854c88bdfecd4
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-01-27 13:51:06 +0000

    Create aliasMap.ts

---
+++ lib/aliasMap.ts
@@ -0,0 +1,11 @@
+export const aliasMap: Record<string, string> = {
+  "praha": "prague",
+  "münchen": "munich",
+  "muenchen": "munich",
+  "roma": "rome",
+  "lisboa": "lisbon",
+  "köln": "cologne",
+  "koln": "cologne",
+  "beograd": "belgrade",
+  "wien": "vienna"
+};
