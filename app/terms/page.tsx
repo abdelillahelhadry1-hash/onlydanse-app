f9c096c6b0ca29b20a39c3697ab9598aa8036344
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-02-04 00:58:07 +0000

    Create page.tsx

---
+++ app/terms/page.tsx
@@ -0,0 +1,8 @@
+export default function TermsPage() {
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h1 className="text-2xl font-semibold mb-4">Terms of Service</h1>
+      <p className="text-gray-600">Our terms will be published here.</p>
+    </div>
+  );
+}
