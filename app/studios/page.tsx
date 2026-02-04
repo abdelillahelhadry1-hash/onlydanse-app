47ae306e736a11b7495eea4d6e6ecf864476539f
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-02-04 00:54:21 +0000

    Create page.tsx

---
+++ app/studios/page.tsx
@@ -0,0 +1,8 @@
+export default function StudiosPage() {
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h1 className="text-2xl font-semibold mb-4">Studios</h1>
+      <p className="text-gray-600">Find dance studios in your area.</p>
+    </div>
+  );
+}
