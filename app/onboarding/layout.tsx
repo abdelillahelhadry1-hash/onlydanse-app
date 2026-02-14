2a3d8c332c7d0f6d6113a199a04526344b560a12
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-02-14 11:26:39 +0000

    Create layout.tsx

---
+++ app/onboarding/layout.tsx
@@ -0,0 +1,7 @@
+export default function OnboardingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="max-w-xl mx-auto px-4 py-10">
+      {children}
+    </div>
+  );
+}
