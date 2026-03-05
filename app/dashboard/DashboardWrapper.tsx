2b930b073c0a047e28b0ee4e14130e5751ecf4de
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-03-05 10:07:34 +0000

    Update DashboardWrapper.tsx

--- app/dashboard/DashboardWrapper.tsx
+++ app/dashboard/DashboardWrapper.tsx
@@ -1,5 +1,11 @@
 import DashboardClient from "./DashboardClient";
 
-export default function DashboardWrapper({ user, roles }: any) {
+export default function DashboardWrapper({
+  user,
+  roles,
+}: {
+  user: any;
+  roles: string[];
+}) {
   return <DashboardClient user={user} roles={roles} />;
 }
