59057022e696598fd43ae2f3482b2380c0d2a82e
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-03-14 10:17:07 +0000

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
 
-export function createSupabaseClient() {
-  return createClient(
+export function supabaseBrowserClient() {
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
